8f3f4f4a5c966487718f616d89aa587950c43f80
ccarcaci <claudiocarcaci@gmail.com>
2022-10-31 18:23:38 +0100

    Fast push

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,4 @@ export function html(filepath: string, options?: Record<string, unknown>): strin
 export function print(ast: Record<string, unknown>): string
 export function parse(filepath: string): Record<string, unknown>
 export function render(contents: string, options?: Record<string, unknown>): string
-export function visit(root: [], visitor: (...args) => unknown | Record<string, unknown>): Record<string, unknown>
+export function visit(root: [], visitor: (...args: unknown[]) => unknown | Record<string, unknown>): Record<string, unknown>
